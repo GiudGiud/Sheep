a3bef5f73b4e0569f2c5c85572a0d07fcba55f1b
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-06-06 16:32:18 -0600

    Add header for diffusion coefficient

---
+++ include/materials/LaplaceYoungDiffusionCoef.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+/**
+ * Diffusion coefficient in Laplace Young
+ */
+class LaplaceYoungDiffusionCoef : public Material
+{
+public:
+  LaplaceYoungDiffusionCoef(const InputParameters & parameters);
+
+  static InputParameters validParams();
+
+protected:
+  virtual void computeQpProperties() override;
+
+  /// The diffusion coefficient
+  ADMaterialProperty<Real> & _k;
+};
